c35fb298410944be0fe30dc94cc9fbec32afc173
Yiyin Zhou <yiyin@ee.columbia.edu>
2018-10-08 14:28:29 -0400

    fixed docker install script for ubuntu

--- scripts/docker-install-ubuntu.sh
+++ scripts/docker-install-ubuntu.sh
@@ -18,7 +18,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 echo Setting up the stable repository
 sudo add-apt-repository \
-   "deb [arch=$(uname -m)] https://download.docker.com/linux/ubuntu \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
