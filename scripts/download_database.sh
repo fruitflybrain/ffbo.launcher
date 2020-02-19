757440aee8c8ad012c6947c3c93348130dbb15b9
Yiyin Zhou <yiyin@ee.columbia.edu>
2020-02-19 17:13:04 -0500

    add download database script and correction in docker-compose.yml

--- scripts/download_database.sh
+++ scripts/download_database.sh
@@ -1,8 +1,9 @@
-# Based on https://github.com/nvidia/nvidia-container-runtime#docker-engine-setup
 
-echo Downloading Database to $HOME
+echo Downloading Database ...
 
-RUN wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1lWCQPw5A6-HwH5oFsGFKDHw7S6JEsvqY' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1lWCQPw5A6-HwH5oFsGFKDHw7S6JEsvqY" -O ffbo_db.tar.gz && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1lWCQPw5A6-HwH5oFsGFKDHw7S6JEsvqY' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1lWCQPw5A6-HwH5oFsGFKDHw7S6JEsvqY" -O ffbo_db.tar.gz && rm -rf /tmp/cookies.txt
 
 tar zxvf ffbo_db.tar.gz
 rm ffbo_db.tar.gz
+
+echo Done.
